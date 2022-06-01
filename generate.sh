08efee454a218597bc0d4cfd1fcb5018466abd5b
zaydam1000000 <86502397+zaypers@users.noreply.github.com>
2022-05-31 18:03:31 -0700

    [GENERATE] added install and update commands

--- generate.sh
+++ generate.sh
@@ -5,9 +5,24 @@ then
   cd /usr/local/share/
   if [ -d "projectgenerator/" ]
   then
-    echo "Deleted lib folder."
-    sudo rm -r projectgenerator/
+    echo "Sorry, but you have already installed this. Please use the 'update' argument to update to the latest version."
+  
+  else
+    sudo mkdir projectgenerator/
+    echo "Created lib folder successfully."
+
   fi
-  echo "Created lib folder."
-  sudo mkdir projectgenerator/
+
+elif [ "$1" == "update" ]
+then
+  cd /usr/local/share/
+  if [ ! -d "projectgenerator/" ]
+  then
+    echo "Sorry, but you have not installed this. Please use the 'install' argument to install this software."
+  
+  else
+    sudo rm -r projectgenerator/
+    sudo mkdir projectgenerator/
+  
+  fi
 fi
