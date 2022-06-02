81c9774539c9e53d07dc57dae0acd7b042dd78f1
zaydam1000000 <86502397+zaypers@users.noreply.github.com>
2022-06-01 18:31:14 -0700

    [INSTALL] added comments

--- install.sh
+++ install.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+# LIBDIR is where the library is located, BASEDIR is where the shell script is located
 LIBDIR="/usr/local/share"
 BASEDIR=$(dirname "$BASH_SOURCE")
 
+# checks if user is root because commands to install need root access
 if [ "$EUID" -ne 0 ]
 then
   echo "Please run as root."
   exit 1
 fi
 
+# checks if project generator is already installed
 if [ -d "$LIBDIR/projectgenerator/" ]
 then
   echo "Project generator has already been installed on your system."
   exit 1
 fi
 
+# creates library folder
 mkdir "$LIBDIR/projectgenerator/"
 echo "Created lib folder."
 mkdir "$LIBDIR/projectgenerator/templates/"
 cp -r "$BASEDIR/templates/" "$LIBDIR/projectgenerator/templates/"
 echo "Added templates to lib folder."
 
+# creates man file
 <<man
 if [ ! -d "$LIBDIR/man/man1" ]
 then
