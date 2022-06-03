fe3a8c273fcfd2c482cd41b39fef88cd31949aa9
zaydam1000000 <86502397+zaypers@users.noreply.github.com>
2022-06-02 17:09:51 -0700

    [UNINSTALL] move to its own file

--- tools/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# removes library folder
-rm -r "/usr/local/share/projectgenerator/"
-echo "Removed lib folder."
-
-# removes man file
-rm "/usr/local/share/man/man1/generate.1"
-echo "Removed man file."
